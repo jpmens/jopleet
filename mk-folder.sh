5ca1b1742151afd2b537cef6f4c312f1df3c0424
Mischa Peters <git@high5.nl>
2020-10-11 14:41:54 +0200

    Update mk-folder.sh
    '[ =]' didn't work for me, changed it to ' = '

--- mk-folder.sh
+++ mk-folder.sh
@@ -6,7 +6,7 @@ foldername="$1"
 
 # try to obtain token from jopleet.config
 
-token="$(awk -F '[ =]'  '/^token/ { print $2 }' jopleet.config)"
+token="$(awk -F ' = '  '/^token/ { print $2 }' jopleet.config)"
 
 curl -sf http://localhost:41184/folders?token=${token} \
 	--data "{ \"title\": \"${foldername}\" }" |
